7f0b95a53c4c2b412f3e11dfe938be7b770a6999
Hugo Gomes <hugomes@gmail.com>
2022-09-04 11:39:03 -0700

    remove secret

--- league_manager_api/settings.py
+++ league_manager_api/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+qmziwvuy+(9fed$i^tu@@l!2w%dvfl_sgpj-j)06-&%e+k#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
